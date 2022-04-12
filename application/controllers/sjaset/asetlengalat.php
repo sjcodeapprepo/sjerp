afe52c7b100fe92f5cfce1d42634a01d68ca4a48
mirzacoder <mirzawargajakarta@gmail.com>
2022-04-12 14:16:41 +0700

    ok

--- application/controllers/sjaset/asetlengalat.php
+++ application/controllers/sjaset/asetlengalat.php
@@ -599,7 +599,7 @@ class Asetlengalat extends Authcontroller
 		$pdf->Text(24, 34, $keterangan1);
 		$pdf->Text(24, 38, $keterangan2);
 		$logo = base_url() . "publicfolder/image/sjlogo_bw2.png";
-		$pdf->Image($logo, 24, 1, 26, 12); //gambar sarana jaya
+		$pdf->Image($logo, 24, 4, 26, 12); //gambar sarana jaya
 		$pdf->Output('lenglat.pdf', 'I');
 	}
 
